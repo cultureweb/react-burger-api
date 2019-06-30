02d36b50c74812dc86e306bbeacb502a7b610fa7
ced <cedric.milius@yahoo.fr>
2019-06-30 17:29:45 +0200

    start project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>My Burger Api</h1>
+
+      </div>
+    );
+  }
+}
+
+export default App;
